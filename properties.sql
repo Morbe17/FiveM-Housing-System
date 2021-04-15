7178c074450d10d96096458548f425e344b50bcb
Resmurf <49992214+Melrpm@users.noreply.github.com>
2021-04-15 09:28:19 -0400

    Default values
    Setting default values on empty table fields.

--- properties.sql
+++ properties.sql
@@ -5,18 +5,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `properties` (
   `id` int(255) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `propertyOwner` varchar(255) NOT NULL,
+  `name` varchar(50),
+  `propertyOwner` varchar(255),
   `interiorId` int(11) NOT NULL,
   `interiorType` int(11) NOT NULL,
   `location` varchar(255) NOT NULL,
   `forSale` tinyint(1) NOT NULL,
-  `salePrice` int(255) NOT NULL,
-  `fee` int(255) NOT NULL,
+  `salePrice` int(255) NOT NULL DEFAULT 0,
+  `fee` int(255) NOT NULL DEFAULT 0,
   `inventory` varchar(255) NOT NULL DEFAULT '[]',
   `capacity` int(255) NOT NULL DEFAULT 0,
-  `locked` tinyint(1) NOT NULL,
-  `previousInt` int(11) NOT NULL
+  `locked` tinyint(1) NOT NULL DEFAULT 0,
+  `previousInt` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 ALTER TABLE `properties`
@@ -30,8 +30,8 @@ COMMIT;
 CREATE TABLE `propertiesuserstatus` (
   `id` int(11) NOT NULL,
   `identifier` varchar(255) NOT NULL,
-  `insideInterior` tinyint(1) NOT NULL,
-  `interiorId` int(11) NOT NULL
+  `insideInterior` tinyint(1) NOT NULL DEFAULT 0,
+  `interiorId` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 ALTER TABLE `propertiesuserstatus`

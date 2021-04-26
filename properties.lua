eae09b1f29fa94b6a3ae05bbbd60ba6524f83cec
LightmasterDanny <45201861+LightmasterDanny@users.noreply.github.com>
2021-04-26 18:10:31 +0200

    Renumber due to double 31

--- properties.lua
+++ properties.lua
@@ -32,65 +32,65 @@ Interiors = {
     {x = -141.3997, y = -620.9006, z = 168.8204, name = "Old Spice Classical", StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 29
     {x = -141.5361, y = -620.9186, z = 168.8204, name = "Old Spice Vintage", StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 30
     {x = -141.392, y = -621.0451, z = 168.8204, name = "Power Broker Ice", StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 31
-    {x = -141.1945, y = -620.8729, z = 168.8204, name = "Power Broker Conservative", StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 31
-    {x = -141.4924, y = -621.0035, z = 168.8205, name = "Power Broker Polished", StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 32
-    {x = -191.0133, y = -579.1428, z = 135.0, name = "Garage 1", StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 33
-    {x = -117.4989, y = -568.1132, z = 135.0, name = "Garage 2", StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 34
-    {x = -136.078, y = -630.1852, z = 135.0, name = "Garage 3", StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 35
-    {x = -146.6166, y = -596.6301, z = 166.0, name = "Mod Shop", StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 36
+    {x = -141.1945, y = -620.8729, z = 168.8204, name = "Power Broker Conservative", StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 32
+    {x = -141.4924, y = -621.0035, z = 168.8205, name = "Power Broker Polished", StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 33
+    {x = -191.0133, y = -579.1428, z = 135.0, name = "Garage 1", StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 34
+    {x = -117.4989, y = -568.1132, z = 135.0, name = "Garage 2", StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 35
+    {x = -136.078, y = -630.1852, z = 135.0, name = "Garage 3", StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 36
+    {x = -146.6166, y = -596.6301, z = 166.0, name = "Mod Shop", StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 37
     --Lom Bank
-    {x = -75.8466, y = -826.9893, z = 243.3859, name = 'Executive Rich', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1}, -- 37
-    {x = -75.49945, y = -827.05, z = 243.386, name = 'Executive Cool', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 38
-    {x = -75.49827, y = -827.1889, z = 243.386, name = 'Executive Contrast', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 39
-    {x = -75.44054, y = -827.1487, z = 243.3859, name = 'Old Spice Warm', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 40
-    {x = -75.63942, y = -827.1022, z = 243.3859, name = 'Old Spice Classical', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 41
-    {x = -75.47446, y = -827.2621, z = 243.386, name = 'Old Spice Vintage', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 42
-    {x = -75.56978, y = -827.1152, z = 243.3859, name = 'Power Broker Ice', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 43
-    {x = -75.51953, y = -827.0786, z = 243.3859, name = 'Power Broker Conservative', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 44
-    {x = -75.41915, y = -827.1118, z = 243.3858, name = 'Power Broker Polished', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 45
-    {x = -84.2193, y = -823.0851, z = 221.0, name = 'Garage 1', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 46
-    {x = -69.8627, y = -824.7498, z = 221.0, name = 'Garage 2', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 47
-    {x = -80.4318, y = -813.2536, z = 221.0, name = 'Garage 3', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 48
-    {x = -73.9039, y = -821.6204, z = 284.0, name = 'Mod Shop', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 49
+    {x = -75.8466, y = -826.9893, z = 243.3859, name = 'Executive Rich', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1}, -- 38
+    {x = -75.49945, y = -827.05, z = 243.386, name = 'Executive Cool', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 39
+    {x = -75.49827, y = -827.1889, z = 243.386, name = 'Executive Contrast', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 40
+    {x = -75.44054, y = -827.1487, z = 243.3859, name = 'Old Spice Warm', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 41
+    {x = -75.63942, y = -827.1022, z = 243.3859, name = 'Old Spice Classical', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 42
+    {x = -75.47446, y = -827.2621, z = 243.386, name = 'Old Spice Vintage', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 43
+    {x = -75.56978, y = -827.1152, z = 243.3859, name = 'Power Broker Ice', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 44
+    {x = -75.51953, y = -827.0786, z = 243.3859, name = 'Power Broker Conservative', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 45
+    {x = -75.41915, y = -827.1118, z = 243.3858, name = 'Power Broker Polished', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 46
+    {x = -84.2193, y = -823.0851, z = 221.0, name = 'Garage 1', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 47
+    {x = -69.8627, y = -824.7498, z = 221.0, name = 'Garage 2', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 48
+    {x = -80.4318, y = -813.2536, z = 221.0, name = 'Garage 3', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 49
+    {x = -73.9039, y = -821.6204, z = 284.0, name = 'Mod Shop', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 50
     --  Maze Bank
-    {x = -1392.667, y = -480.4736, z = 72.04217, name = 'Executive Rich', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 50
-    {x = -1392.542, y = -480.4011, z = 72.04211, name = 'Executive Cool', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 51
-    {x = -1392.626, y = -480.4856, z = 72.04212, name = 'Executive Contrast', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 52
-    {x = -1392.617, y = -480.6363, z = 72.04208, name = 'Old Spice Warm', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 53
-    {x = -1392.532, y = -480.7649, z = 72.04207, name = 'Old Spice Classical', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 54
-    {x = -1392.611, y = -480.5562, z = 72.04214, name = 'Old Spice Vintage', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 55
-    {x = -1392.563, y = -480.549, z = 72.0421, name = 'Power Broker Ice', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 56
-    {x = -1392.528, y = -480.475, z = 72.04206, name = 'Power Broker Convservative', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 57
-    {x = -1392.416, y = -480.7485, z = 72.04207, name = 'Power Broker Polished', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 58
-    {x = -1388.84, y = -478.7402, z = 56.1, name = 'Garage 1', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 59
-    {x = -1388.86, y = -478.7574, z = 48.1, name = 'Garage 2', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 60
-    {x = -1374.682, y = -474.3586, z = 56.1, name = 'Garage 3', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 61
-    {x = -1391.245, y = -473.9638, z = 77.2, name = 'Mod Shop', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 62
+    {x = -1392.667, y = -480.4736, z = 72.04217, name = 'Executive Rich', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 51
+    {x = -1392.542, y = -480.4011, z = 72.04211, name = 'Executive Cool', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 52
+    {x = -1392.626, y = -480.4856, z = 72.04212, name = 'Executive Contrast', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 53
+    {x = -1392.617, y = -480.6363, z = 72.04208, name = 'Old Spice Warm', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 54
+    {x = -1392.532, y = -480.7649, z = 72.04207, name = 'Old Spice Classical', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 55
+    {x = -1392.611, y = -480.5562, z = 72.04214, name = 'Old Spice Vintage', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 56
+    {x = -1392.563, y = -480.549, z = 72.0421, name = 'Power Broker Ice', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 57
+    {x = -1392.528, y = -480.475, z = 72.04206, name = 'Power Broker Convservative', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 58
+    {x = -1392.416, y = -480.7485, z = 72.04207, name = 'Power Broker Polished', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 59
+    {x = -1388.84, y = -478.7402, z = 56.1, name = 'Garage 1', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 60
+    {x = -1388.86, y = -478.7574, z = 48.1, name = 'Garage 2', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 61
+    {x = -1374.682, y = -474.3586, z = 56.1, name = 'Garage 3', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 62
+    {x = -1391.245, y = -473.9638, z = 77.2, name = 'Mod Shop', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 63
     -- Club Houses
-    {x = 1107.04, y = -3157.399, z = -37.51859, name = 'Clubhouse 1', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 63 
-    {x = 998.4809, y = -3164.711, z = -38.90733, name = 'Clubhouse 2', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 64
-    {x = 997.0813, y = -3200.733, z = -36.39375, name = 'Meth Lab', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 65
-    {x = 1051.491, y = -3196.536, z = -39.14842, name = 'Weed Farm', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 66
-    {x = 1093.6, y = -3196.6, z = -38.99841, name = 'Cocaine Lockup', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 67
-    {x = 1121.897, y = -3195.338, z = -40.4025, name = 'Counterfeit Cash Factory', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 68
-    {x = 1165, y = -3196.6, z = -39.01306, name = 'Document Forgery Office', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 69
-    {x = 1094.988, y = -3101.776, z = -39.00363, name = 'Warehouse Small', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 70
-    {x = 1056.486, y = -3105.724, z = -39.00439, name = 'Warehouse Medium', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 71
-    {x = 1006.967, y = -3102.079, z = -39.0035, name = 'Warehouse Large', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 72
-    {x = 994.5925, y = -3002.594, z = -39.64699, name = 'Vehicle Warehouse', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 73
-    {x = 982.0083, y = -100.8747, z = 74.84512, name = 'Lost MC Clubhouse', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 74
+    {x = 1107.04, y = -3157.399, z = -37.51859, name = 'Clubhouse 1', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 64 
+    {x = 998.4809, y = -3164.711, z = -38.90733, name = 'Clubhouse 2', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 65
+    {x = 997.0813, y = -3200.733, z = -36.39375, name = 'Meth Lab', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 66
+    {x = 1051.491, y = -3196.536, z = -39.14842, name = 'Weed Farm', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 67
+    {x = 1093.6, y = -3196.6, z = -38.99841, name = 'Cocaine Lockup', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 68
+    {x = 1121.897, y = -3195.338, z = -40.4025, name = 'Counterfeit Cash Factory', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 69
+    {x = 1165, y = -3196.6, z = -39.01306, name = 'Document Forgery Office', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 70
+    {x = 1094.988, y = -3101.776, z = -39.00363, name = 'Warehouse Small', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 71
+    {x = 1056.486, y = -3105.724, z = -39.00439, name = 'Warehouse Medium', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 72
+    {x = 1006.967, y = -3102.079, z = -39.0035, name = 'Warehouse Large', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 73
+    {x = 994.5925, y = -3002.594, z = -39.64699, name = 'Vehicle Warehouse', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 74
+    {x = 982.0083, y = -100.8747, z = 74.84512, name = 'Lost MC Clubhouse', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 75
     -- Special Locations
-    {x = -163.3628, y = -2385.161, z = 5.999994, name = 'Normal Cargo Ship', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 75
-    {x = -163.3628, y = -2385.161, z = 5.999994, name = 'Sunken Cargo Ship', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 76
-    {x = -163.3628, y = -2385.161, z = 5.999994, name = 'Burning Cargo Ship', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 77
-    {x = 300.5927, y = 300.5927, z = 104.3776, name = 'Red Carpet', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 78
-    {x = -1020.518, y = 663.27, z = 153.5167, name = 'Rekt Stilthouse Destroyed', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 79
-    {x = -1020.518, y = 663.27, z = 153.5167, name = 'Rekt Stilthouse Rebuild', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 80
-    {x = 2.6968, y = -667.0166, z = 16.13061, name = 'Union Depository', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 81
-    {x = 1975.552, y = 3820.538, z = 33.44833, name = 'Trevors Trailer Dirty', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 82
-    {x = 1975.552, y = 3820.538, z = 33.44833, name = 'Trevors Trailer Clean', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 83
-    {x = -248.6731, y = -2010.603, z = 30.14562, name = 'Stadium', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 84
-    {x = -585.8247, y = -282.72, z = 35.45475, name = 'Max Renda Shop', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 85
-    {x = -630.07, y = -236.332, z = 38.05704, name = 'Jewel Store', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 86 -- Do not use
-    {x = 110.4, y = -744.2, z = 45.7496, name = 'FIB Lobby', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1}-- 87
+    {x = -163.3628, y = -2385.161, z = 5.999994, name = 'Normal Cargo Ship', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 76
+    {x = -163.3628, y = -2385.161, z = 5.999994, name = 'Sunken Cargo Ship', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 77
+    {x = -163.3628, y = -2385.161, z = 5.999994, name = 'Burning Cargo Ship', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 78
+    {x = 300.5927, y = 300.5927, z = 104.3776, name = 'Red Carpet', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 79
+    {x = -1020.518, y = 663.27, z = 153.5167, name = 'Rekt Stilthouse Destroyed', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 80
+    {x = -1020.518, y = 663.27, z = 153.5167, name = 'Rekt Stilthouse Rebuild', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 81
+    {x = 2.6968, y = -667.0166, z = 16.13061, name = 'Union Depository', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 82
+    {x = 1975.552, y = 3820.538, z = 33.44833, name = 'Trevors Trailer Dirty', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 83
+    {x = 1975.552, y = 3820.538, z = 33.44833, name = 'Trevors Trailer Clean', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 84
+    {x = -248.6731, y = -2010.603, z = 30.14562, name = 'Stadium', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 85
+    {x = -585.8247, y = -282.72, z = 35.45475, name = 'Max Renda Shop', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 86
+    {x = -630.07, y = -236.332, z = 38.05704, name = 'Jewel Store', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1},-- 87 -- Do not use
+    {x = 110.4, y = -744.2, z = 45.7496, name = 'FIB Lobby', StorageX = 1, StorageY = 1, StorageY = 1, wardrobeX = 1, wardrobeY = 1, wardrobeZ = 1}-- 88
 }
